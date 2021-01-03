8a353640590bd272672134e2db286e176683ddad
Henrico Leodra <leodra28@gmail.com>
2021-01-03 22:17:09 +0700

    [ADD] Database Connection

---
+++ database.js
@@ -0,0 +1,42 @@
+let mysql = require("mysql");
+
+// MySQL db Connection
+let connection = mysql.createPool({
+  connectionLimit: 10,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "eviteu",
+});
+
+function executeQuery(sql, callback) {
+  connection.getConnection((err, connection) => {
+    if (err) {
+      return callback(err, null);
+    } else {
+      if (connection) {
+        connection.query(sql, function (error, results, fields) {
+          connection.release();
+          if (error) {
+            return callback(error, null);
+          }
+          return callback(null, results);
+        });
+      }
+    }
+  });
+}
+
+function query(sql, callback) {
+  executeQuery(sql, function (err, data) {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, data);
+  });
+}
+
+module.exports = {
+  query: query,
+  connection: connection,
+};
