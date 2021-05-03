2178ba73810799aea1864ba6d2d31575c6f5c81b
Henrico Leodra <leodra28@gmail.com>
2021-05-04 00:07:53 +0700

    [ADD] Text Color on Event

--- app/Models/Event.js
+++ app/Models/Event.js
@@ -10,9 +10,10 @@ class Event {
     this.time = data.time;
     this.location = data.location;
     this.coordinates = data.coordinates;
-    this.eventPrimary = data.eventPrimary;
-    this.eventSecondary = data.eventSecondary;
-    this.eventAccent = data.eventAccent;
+    this.primaryColor = data.primaryColor;
+    this.secondaryColor = data.secondaryColor;
+    this.accentColor = data.accentColor;
+    this.textColor = data.textColor;
     this.max = data.max;
     this.idHost = data.idHost;
     this.idType = data.idType;
@@ -20,15 +21,15 @@ class Event {
 
   addEvent(callback) {
     db.query(
-      `INSERT INTO eviteu_event(eventTitle, eventSubTitle, eventDescription, eventHighlight, date, time, location, coordinates, eventPrimary, eventSecondary,  eventAccent, max, idHost
-        , idType) VALUES('${this.eventTitle}', '${this.eventSubTitle}', '${this.eventDescription}', '${this.eventHighlight}', '${this.date}', '${this.time}', '${this.location}', '${this.coordinates}', '${this.eventPrimary}', '${this.eventSecondary}', '${this.eventAccent}', ${this.max}, ${this.idHost}, ${this.idType})`,
+      `INSERT INTO eviteu_event(eventTitle, eventSubTitle, eventDescription, eventHighlight, date, time, location, coordinates, primaryColor, secondaryColor,  accentColor, textColor, max, idHost
+        , idType) VALUES('${this.eventTitle}', '${this.eventSubTitle}', '${this.eventDescription}', '${this.eventHighlight}', '${this.date}', '${this.time}', '${this.location}', '${this.coordinates}', '${this.primaryColor}', '${this.secondaryColor}', '${this.accentColor}', '${this.textColor}', ${this.max}, ${this.idHost}, ${this.idType})`,
       callback
     );
   }
 
   updateEvent(idEvent, callback) {
     db.query(
-      `UPDATE eviteu_event SET eventTitle = '${this.eventTitle}', eventSubTitle = '${this.eventSubTitle}', eventDescription = '${this.eventDescription}', eventHighlight = '${this.eventHighlight}', date = '${this.date}', time = '${this.time}', location = '${this.location}', coordinates = '${this.coordinates}', eventPrimary = '${this.eventPrimary}', eventSecondary = '${this.eventSecondary}', eventAccent = '${this.eventAccent}', max = ${this.max}, idType = ${this.idType} WHERE idevent = ${idEvent}`,
+      `UPDATE eviteu_event SET eventTitle = '${this.eventTitle}', eventSubTitle = '${this.eventSubTitle}', eventDescription = '${this.eventDescription}', eventHighlight = '${this.eventHighlight}', date = '${this.date}', time = '${this.time}', location = '${this.location}', coordinates = '${this.coordinates}', primaryColor = '${this.primaryColor}', secondaryColor = '${this.secondaryColor}', accentColor = '${this.accentColor}', textColor = '${this.textColor}', max = ${this.max}, idType = ${this.idType} WHERE idevent = ${idEvent}`,
       callback
     );
   }
@@ -50,14 +51,14 @@ class Event {
 
   static getAllEventByIdHost(idHost, callback) {
     db.query(
-      `SELECT eviteu_event.idEvent, eventTitle, eventSubTitle, eventDescription, eventHighlight, date, time, location, coordinates, eventPrimary, eventSecondary, eventAccent, max, idHost, typeName, COUNT(idGuest) AS totalGuestInvited,SUM(status) AS totalGuestRsvp,SUM(qty) AS totalGuestBrought, SUM(attend) AS totalGuestAttended FROM eviteu_guest RIGHT OUTER JOIN eviteu_event ON eviteu_guest.idEvent = eviteu_event.idEvent INNER JOIN eviteu_type ON eviteu_event.idType = eviteu_type.idType WHERE idHost = ${idHost} GROUP BY idEvent`,
+      `SELECT eviteu_event.idEvent, eventTitle, eventSubTitle, eventDescription, eventHighlight, date, time, location, coordinates, primaryColor, secondaryColor, accentColor, textColor, max, idHost, typeName, COUNT(idGuest) AS totalGuestInvited,SUM(status) AS totalGuestRsvp,SUM(qty) AS totalGuestBrought, SUM(attend) AS totalGuestAttended FROM eviteu_guest RIGHT OUTER JOIN eviteu_event ON eviteu_guest.idEvent = eviteu_event.idEvent INNER JOIN eviteu_type ON eviteu_event.idType = eviteu_type.idType WHERE idHost = ${idHost} GROUP BY idEvent`,
       callback
     );
   }
 
   static getAllEventByIdCommittee(idCommittee, callback) {
     db.query(
-      `SELECT eviteu_event.idEvent, eventTitle, eventSubTitle, eventDescription, eventHighlight, date, time, location, coordinates, eventPrimary, eventSecondary, eventAccent, max, idHost, typeName FROM eviteu_manage INNER JOIN eviteu_event ON eviteu_manage.idEvent = eviteu_event.idEvent INNER JOIN eviteu_type ON eviteu_event.idType = eviteu_type.idType WHERE idCommittee = ${idCommittee}`,
+      `SELECT eviteu_event.idEvent, eventTitle, eventSubTitle, eventDescription, eventHighlight, date, time, location, coordinates, primaryColor, secondaryColor, accentColor, textColor, max, idHost, typeName FROM eviteu_manage INNER JOIN eviteu_event ON eviteu_manage.idEvent = eviteu_event.idEvent INNER JOIN eviteu_type ON eviteu_event.idType = eviteu_type.idType WHERE idCommittee = ${idCommittee}`,
       callback
     );
   }
