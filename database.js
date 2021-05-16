8735de58637a03638011adc7b245fc20747dde9b
Henrico Leodra <leodra28@gmail.com>
2021-05-16 21:17:21 +0700

    [UPDATE] .env Variables for DB

--- database.js
+++ database.js
@@ -3,9 +3,9 @@ let mysql = require("mysql");
 // MySQL db Connection
 let connection = mysql.createPool({
   connectionLimit: 10,
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: "eviteu",
 });
 
