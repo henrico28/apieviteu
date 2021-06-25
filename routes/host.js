7aed051d4d5c945b0944412ebed0263cf3396dbe
Henrico Leodra <leodra28@gmail.com>
2021-06-25 11:18:48 +0700

    [UPDATE] remove unused function

--- routes/host.js
+++ routes/host.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var router = express.Router();
 var useController = require("../app/Controllers/HostController");
-var auth = require("../app/Controllers/AuthController");
 
 router.get("/lists", useController.getAllHost);
 router.post("/create", useController.createHost);
